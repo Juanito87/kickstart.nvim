199d8fab5dc3025e42ae9bbb53ba2534de62795b
juanito87 <juancruz.f87@gmail.com>
2024-04-22 23:22:40 -0300

    Updating plugins config

--- lua/plugins_config/worktree.lua
+++ lua/plugins_config/worktree.lua
@@ -1,3 +1,7 @@
-require('git-worktree').setup {
-  change_directory_command = 'tcd', -- command to change directory (e.g. lcd for NvimTree)
+return {
+  'ThePrimeagen/git-worktree.nvim', -- Manage worktrees from nvim
+  opts = {
+    change_directory_command = 'tcd', -- command to change directory (e.g. lcd for NvimTree)
+    autopush = true,
+        },
 }
