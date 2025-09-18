088e844145ea9a413d968d2f6072ad7df178060a
Juanito <11276687+Juanito87@users.noreply.github.com>
2025-09-18 16:10:33 -0300

    Update lsp.lua
    Fix typo

--- lua/plugins_config/lsp.lua
+++ lua/plugins_config/lsp.lua
@@ -268,7 +268,7 @@ return {
         ensure_installed = ensure_installed,
         auto_update = false,
       }
-      local null_ls = require("null-ls")
+      local null_ls = require("null_ls")
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
