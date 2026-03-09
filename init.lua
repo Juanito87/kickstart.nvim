ab2d84207cd5e0e11c93bd28c16357ba01d25633
Juanito <11276687+Juanito87@users.noreply.github.com>
2026-03-09 19:58:15 -0300

    Remove extraneous character from init.lua

--- init.lua
+++ init.lua
@@ -64,7 +64,6 @@ Kickstart Guide:
 
     MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
     which is very useful when you're not exactly sure of what you're looking for.
-g
   I have left several `:help X` comments throughout the init.lua
     These are hints about where to find more information about the relevant settings,
     plugins or Neovim features used in Kickstart.
