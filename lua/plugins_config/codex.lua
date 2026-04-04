9a4679fea36d828267b6fde97bef8ae030990c40
juanito87 <juancruz.f87@gmail.com>
2026-04-04 15:06:05 -0300

    Adding codex

---
+++ lua/plugins_config/codex.lua
@@ -0,0 +1,56 @@
+-- [[ Codex CLI integration ]]
+-- Opens codex CLI in a vertical terminal split within Neovim.
+-- No API key needed here if the Codex CLI is already authenticated.
+
+local codex_buf = nil
+local codex_win = nil
+
+local function is_valid()
+  return codex_buf
+    and vim.api.nvim_buf_is_valid(codex_buf)
+    and codex_win
+    and vim.api.nvim_win_is_valid(codex_win)
+end
+
+local function open()
+  vim.cmd 'vsplit'
+  vim.cmd 'terminal codex'
+  codex_buf = vim.api.nvim_get_current_buf()
+  codex_win = vim.api.nvim_get_current_win()
+  vim.cmd 'startinsert'
+end
+
+local function toggle()
+  if codex_win and vim.api.nvim_win_is_valid(codex_win) then
+    vim.api.nvim_win_close(codex_win, false)
+    codex_win = nil
+  elseif codex_buf and vim.api.nvim_buf_is_valid(codex_buf) then
+    vim.cmd 'vsplit'
+    codex_win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(codex_win, codex_buf)
+    vim.cmd 'startinsert'
+  else
+    open()
+  end
+end
+
+local function send_selection()
+  local start_pos = vim.fn.getpos "'<"
+  local end_pos = vim.fn.getpos "'>"
+  local lines = vim.fn.getline(start_pos[2], end_pos[2])
+  if #lines > 0 then
+    lines[#lines] = string.sub(lines[#lines], 1, end_pos[3])
+    lines[1] = string.sub(lines[1], start_pos[3])
+  end
+  local text = table.concat(lines, '\n')
+
+  if not is_valid() then
+    open()
+  end
+
+  local chan = vim.bo[codex_buf].channel
+  vim.api.nvim_chan_send(chan, text .. '\n')
+end
+
+vim.keymap.set('n', '<leader>cot', toggle, { desc = '[C]ode [O]penAI [T]oggle' })
+vim.keymap.set('v', '<leader>cos', send_selection, { desc = '[C]ode [O]penAI [S]end' })
